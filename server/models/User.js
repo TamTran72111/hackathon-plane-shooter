9db254da5fd6f5627521615c5fddefb2dcf0dbb8
TamTran72111 <tam.tran72111@gmail.com>
2021-01-22 19:42:58 -0500

    Add User model

---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = new mongoose.model('User', UserSchema);
+
+const SALT_ROUNDS = 10;
+
+User.signUp = async (username, password) => {
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const user = new User({
+    username,
+    password: hashedPassword,
+  });
+  return user.save();
+};
+
+User.login = async (username, password) => {
+  const user = await User.findOne({ username });
+  return await bcrypt.compare(password, user.password);
+};
+
+module.exports = User;
